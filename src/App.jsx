b166b55855d56bbfd473e393753c02e239e8c1c7
japg01 <japerezg.2001@gmail.com>
2025-10-24 01:19:46 -0400

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import { LandingPage } from "./pages/landing_page";
 import { AboutPage } from "./pages/about_page";
 import ContactPage from "./pages/contact_page";
 
+const rawBase = import.meta.env.BASE_URL || "/";
+const basename = rawBase.replace(/\/$/, "");
 
 function App() {
   return (
-    <Router>
+    <Router basename={basename}>
       <Header />
       <main className="w-full mt-0">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage/>} />
-
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
     </Router>
